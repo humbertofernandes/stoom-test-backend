25cbf55420489c1d2299ce35499db6c5b25224c4
beto <humbertotpgfernandes@gmail.com>
2020-07-05 18:32:09 -0300

    Google API

--- src/main/java/br/com/humbertofernandes/stoom/api/service/impl/AddressServiceImpl.java
+++ src/main/java/br/com/humbertofernandes/stoom/api/service/impl/AddressServiceImpl.java
@@ -4,9 +4,16 @@ import br.com.humbertofernandes.stoom.api.model.Address;
 import br.com.humbertofernandes.stoom.api.repository.AddressRepository;
 import br.com.humbertofernandes.stoom.api.service.AddressService;
 import br.com.humbertofernandes.stoom.api.service.exception.AddressNotFoundException;
+import com.google.maps.GeoApiContext;
+import com.google.maps.GeocodingApi;
+import com.google.maps.model.GeocodingResult;
+import com.google.maps.model.LatLng;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,6 +23,8 @@ public class AddressServiceImpl implements AddressService {
 
     private final AddressRepository repository;
 
+    private Logger LOG = LoggerFactory.getLogger(AddressServiceImpl.class);
+
     @Autowired
     public AddressServiceImpl(AddressRepository repository) {
         this.repository = repository;
@@ -23,7 +32,32 @@ public class AddressServiceImpl implements AddressService {
 
     @Override
     public Address save(Address address) {
+        verifyLatitudeAndLongitude(address);
         return repository.save(address);
+    }
+
+    private void verifyLatitudeAndLongitude(Address address) {
+        if (StringUtils.isEmpty(address.getLatitude()) || StringUtils.isEmpty(address.getLongitude())) {
+            try {
+                StringBuffer addressStr = new StringBuffer();
+                addressStr.append(address.getNumber());
+                addressStr.append(address.getStreetName());
+                addressStr.append(address.getNeighbourhood());
+                addressStr.append(address.getCity());
+                addressStr.append(address.getState());
+                addressStr.append(address.getCountry());
+
+                GeoApiContext context = new GeoApiContext.Builder()
+                        .apiKey("AIzaSyBm3xh9oZP1ksMWcMzVaZQevWlrtb8tIgc")
+                        .build();
+                GeocodingResult[] results = GeocodingApi.geocode(context, addressStr.toString()).await();
+                LatLng location = results[0].geometry.location;
+                address.setLatitude(String.valueOf(location.lat));
+                address.setLongitude(String.valueOf(location.lng));
+            } catch (Exception e) {
+                LOG.error("Error get location address");
+            }
+        }
     }
 
     @Override
